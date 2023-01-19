d0cc2a1cbce4ee1348f35dbc465d8156dd4b1392
Enzo Soares <enzoayrsoares@gmail.com>
2023-01-19 21:04:56 +0100

    feat: adding likes and wishlist activities

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.androidproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidproject.databinding.ActivityHomeBinding
 
@@ -12,5 +14,24 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initLikeButtonBehavior();
+        initWishlistButtonBehavior();
+    }
+
+    private fun initLikeButtonBehavior() {
+        val likesButton: ImageView = findViewById(R.id.likesButton)
+        likesButton.setOnClickListener{
+            val likesActivity = Intent(this, LikesActivity::class.java)
+            startActivity(likesActivity)
+        }
+    }
+
+    private fun initWishlistButtonBehavior() {
+        val wishlistButton: ImageView = findViewById(R.id.wishlistButton)
+        wishlistButton.setOnClickListener{
+            val wishlistActivity = Intent(this, WishlistActivity::class.java)
+            startActivity(wishlistActivity)
+        }
     }
 }
