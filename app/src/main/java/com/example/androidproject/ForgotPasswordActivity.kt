87f5534d733b3eb65c74496da1267e00cb498ebe
Loïc Vanden Bossche <loloic066@gmail.com>
2023-01-29 06:18:28 +0100

    feat: added error handling for reset_password

--- app/src/main/java/com/example/androidproject/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/androidproject/ForgotPasswordActivity.kt
@@ -1,9 +1,13 @@
 package com.example.androidproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
@@ -21,7 +25,34 @@ class ForgotPasswordActivity : AppCompatActivity() {
 
         initializeUI()
 
+        emailTV.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+            override fun afterTextChanged(editable: Editable) {
+                fieldResetError(emailTV)
+            }
+        })
+
         initSubmitButtonBehavior()
+    }
+
+    private fun fieldSetError(field: EditText, error: String) {
+        val icon = ResourcesCompat.getDrawable(resources, R.drawable.warning, null)
+
+        icon?.setBounds(
+            0, 0,
+            icon.intrinsicWidth,
+            icon.intrinsicHeight
+        )
+
+        field.setError(error, icon)
+
+        field.setBackgroundResource(R.drawable.auth_field_bg_error)
+    }
+
+    private fun fieldResetError(field: EditText) {
+        field.error = null
+        field.setBackgroundResource(R.drawable.auth_field_bg)
     }
 
     private fun initSubmitButtonBehavior() {
@@ -33,7 +64,12 @@ class ForgotPasswordActivity : AppCompatActivity() {
         val email = emailTV.text.toString()
 
         if (email.isEmpty()) {
-            Toast.makeText(applicationContext, getString(R.string.auth_no_email), Toast.LENGTH_LONG).show()
+            fieldSetError(emailTV, getString(R.string.auth_no_email))
+            return
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailTV.text).matches()) {
+            fieldSetError(emailTV, getString(R.string.auth_bad_email))
             return
         }
 
@@ -44,9 +80,11 @@ class ForgotPasswordActivity : AppCompatActivity() {
                 progressBar.visibility = View.GONE
 
                 if (task.isSuccessful) {
-                    Toast.makeText(applicationContext, "Code sent", Toast.LENGTH_LONG).show()
+                    Toast.makeText(applicationContext, getString(R.string.forgot_password_successful), Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
                 } else {
-                    Toast.makeText(applicationContext, "Code send failed", Toast.LENGTH_LONG).show()
+                    Toast.makeText(applicationContext, getString(R.string.forgot_password_failed), Toast.LENGTH_LONG).show()
                 }
             }
     }
