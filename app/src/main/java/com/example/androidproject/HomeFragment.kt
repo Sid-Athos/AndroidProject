7cb9834c86c51d48abbfd306671bb1a4a3a612db
Enzo Soares <enzoayrsoares@gmail.com>
2023-01-30 14:29:19 +0100

    feat: adding home fragment

--- app/src/main/java/com/example/androidproject/HomeFragment.kt
+++ app/src/main/java/com/example/androidproject/HomeFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class HomeFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textView: TextView = requireView().findViewById(R.id.title_home_fragment)
+        textView.setText("c bon")
     }
 }
