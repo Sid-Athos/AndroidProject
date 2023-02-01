d38e20587a89424554512676be430c58e1e2da0d
Enzo Soares <enzoayrsoares@gmail.com>
2023-02-01 12:07:02 +0100

    feat: recycler view is working

--- app/src/main/java/com/example/androidproject/HomeFragment.kt
+++ app/src/main/java/com/example/androidproject/HomeFragment.kt
@@ -6,11 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textView: TextView = requireView().findViewById(R.id.title_home_fragment)
-        textView.setText("c bon")
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.game_list_view)
+        val color = resources.getColor(R.color.white)
+        recyclerView.setBackgroundColor(color)
+        val games = listOf("2240","970","821","10","2240","970","821","10","2240","970","821","10","2240","970","821","10")
+        recyclerView.adapter = GameCardList(games, true)
     }
 }
