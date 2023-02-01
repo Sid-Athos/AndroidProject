808af0276e5ab574188d4f4858e5b4669319e643
Sid-Athos <sa.bennaceur@gmail.com>
2023-02-01 05:04:06 +0100

    clc le xml

--- app/src/main/java/com/example/androidproject/fragments/GameCard.kt
+++ app/src/main/java/com/example/androidproject/fragments/GameCard.kt
@@ -1,18 +1,14 @@
-package com.example.androidproject
+package com.example.androidproject.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.androidproject.R
 
-class GameCardFragment (val id: String, val displayDetails: Boolean) : Fragment(R.layout.fragment_game_card) {
+class GameCard (val id: String, val displayDetails: Boolean) : Fragment(R.layout.fragment_game_card) {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game_card, container, false)
-    }
-
-    companion object {
-        fun newInstance() = GameCardFragment()
     }
 }
