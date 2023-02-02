e58eff2533e408d55cd94351a5dd1cf597078a24
Enzo Soares <enzoayrsoares@gmail.com>
2023-02-02 08:33:14 +0100

    feat: adding img cover

--- app/src/main/java/com/example/androidproject/fragments/GameCardList.kt
+++ app/src/main/java/com/example/androidproject/fragments/GameCardList.kt
@@ -1,20 +1,24 @@
 package com.example.androidproject.fragments
 
-import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidproject.services.SteamApiService
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import com.example.androidproject.R
 import com.example.androidproject.models.Game
+import com.google.gson.JsonElement
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.*
+import java.net.URL
 
 class GameCardList(private val games: List<String>, private val displayDetails: Boolean): RecyclerView.Adapter<GameCardList.ViewHolder>() {
     private val api = Retrofit.Builder()
@@ -25,20 +29,24 @@ class GameCardList(private val games: List<String>, private val displayDetails: 
         .create(SteamApiService::class.java)
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val coverView: ImageView
         private val titleView: TextView
         private val studioNameView: TextView
         private val priceView: TextView
 
         init {
+            coverView = itemView.findViewById(R.id.game_cover)
             titleView = itemView.findViewById(R.id.game_title)
             studioNameView = itemView.findViewById(R.id.game_studio_name)
             priceView = itemView.findViewById(R.id.game_price)
         }
 
         fun bind(data: Game) {
+            coverView.setImageBitmap(data.cover)
             titleView.text = data.title
             studioNameView.text = data.studio
             priceView.text = itemView.resources.getString(R.string.gamePrice, data.price)
+            Log.v("Game Card Bind:", data.cover.toString())
         }
     }
 
@@ -50,11 +58,19 @@ class GameCardList(private val games: List<String>, private val displayDetails: 
     @OptIn(DelicateCoroutinesApi::class)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val gameId = games[position]
+        val coverUrl = URL("https://steamcdn-a.akamaihd.net/steam/apps/$gameId/library_600x900.jpg")
 
         GlobalScope.launch(Dispatchers.Main) {
             try {
-                val data = withContext(Dispatchers.IO) { api.getAppById(gameId).await() }
-                holder.bind(Game(gameId, data))
+                var imageBitmap: Bitmap
+                var data: JsonElement
+
+                withContext(Dispatchers.IO) {
+                    imageBitmap = BitmapFactory.decodeStream(coverUrl.openStream())
+                    data = api.getAppById(gameId).await()
+                }
+
+                holder.bind(Game(gameId, data, imageBitmap))
             } catch (e: Exception) { Log.e("Game Card Bind:", e.toString()) }
         }
     }
--- app/src/main/java/com/example/androidproject/models/Game.kt
+++ app/src/main/java/com/example/androidproject/models/Game.kt
@@ -1,11 +1,13 @@
 package com.example.androidproject.models
 
+import android.graphics.Bitmap
 import com.google.gson.JsonElement
 
-data class Game(val id:String, var title:String, var studio:String, var price:String) {
-    constructor(id:String, json: JsonElement):
+data class Game(val id:String, val title:String, val studio:String, val price:String, val cover: Bitmap ) {
+    constructor(id:String, json: JsonElement, cover: Bitmap):
             this(id,
                 json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("name").asString,
                 json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("publishers").asJsonArray.get(0).asString,
-                json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("price_overview").asJsonObject.get("final_formatted").asString)
+                json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("price_overview").asJsonObject.get("final_formatted").asString,
+                cover)
 }
