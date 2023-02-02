7482173a84ab990a25ed98df7434f85235851c40
Sid B <sid.bennaceur@haidi.io>
2023-02-02 08:33:22 +0100

    fix/home not displaying

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.androidproject
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
