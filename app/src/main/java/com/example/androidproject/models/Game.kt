6af0baf7b26f41627d13ffc585bc4317b46ae36c
Enzo Soares <enzoayrsoares@gmail.com>
2023-02-02 20:34:40 +0100

    fix: removing bitmap from cache

--- app/src/main/java/com/example/androidproject/models/Game.kt
+++ app/src/main/java/com/example/androidproject/models/Game.kt
@@ -2,14 +2,15 @@ package com.example.androidproject.models
 
 import android.graphics.Bitmap
 import com.google.gson.JsonElement
+import java.net.URL
 
-data class Game(val id:String, val title:String, val studio:String, val price:String, val cover: Bitmap ) {
-    constructor(id:String, json: JsonElement, cover: Bitmap):
-            this(id,
-                json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("name").asString,
-                json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("publishers").asJsonArray.get(0).asString,
-                if(json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("is_free").asBoolean) "free"
-                else if (!json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.has("price_overview")) "free"
-                else json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("price_overview").asJsonObject.get("final_formatted").asString,
-                cover)
+data class Game(val id:String, val title:String, val studio:String, val price:String, val coverUrl: URL, val backgroundUrl: URL ) {
+    constructor(id:String, json: JsonElement): this(id,
+        json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("name").asString,
+        json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("publishers").asJsonArray.get(0).asString,
+        if(json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("is_free").asBoolean) "free"
+        else if (!json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.has("price_overview")) "free"
+        else json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("price_overview").asJsonObject.get("final_formatted").asString,
+        URL("https://steamcdn-a.akamaihd.net/steam/apps/$id/library_600x900.jpg\n"),
+        URL(json.asJsonObject.get(id).asJsonObject.get("data").asJsonObject.get("background_raw").asString))
 }
