260d6eaac7759601d19faf014a98e8ed77b984f9
Loïc Vanden Bossche <loloic066@gmail.com>
2023-02-03 03:02:35 +0100

    feat: added navigation binding for likes/wishes

--- app/src/main/java/com/example/androidproject/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/androidproject/fragments/home/HomeFragment.kt
@@ -3,7 +3,9 @@ package com.example.androidproject.fragments.home
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.core.widget.doOnTextChanged
+import androidx.navigation.fragment.findNavController
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 import com.example.androidproject.R
@@ -15,9 +17,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         val searchBar: EditText = view.findViewById(R.id.search_bar)
 
-        val bestsellerContainer:FragmentContainerView = view.findViewById(R.id.bestseller_container)
-        val searchContainer:FragmentContainerView = view.findViewById(R.id.search_container)
+        val bestsellerContainer: FragmentContainerView = view.findViewById(R.id.bestseller_container)
+        val searchContainer: FragmentContainerView = view.findViewById(R.id.search_container)
         val searchFragment = childFragmentManager.findFragmentById(R.id.search_container) as Search
+
+        view.findViewById<ImageView>(R.id.likesButton).setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLikesFragment()
+            findNavController().navigate(action)
+        }
+
+        view.findViewById<ImageView>(R.id.wishlistButton).setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToWishlistFragment()
+            findNavController().navigate(action)
+        }
 
         var displayedSearch = false
 
